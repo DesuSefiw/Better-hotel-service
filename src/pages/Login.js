d814b2c9d227689d7303b1acc7be25801ebaa2d5
Desalegn Sefiw <166336863+DesuSefiw@users.noreply.github.com>
2025-05-13 13:34:10 +0300

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,7 +7,7 @@ const Login = () => {
   const navigate = useNavigate();
 
   const handleLogin = async () => {
-    const res = await fetch('http://localhost:5000/api/login', {
+    const res = await fetch('https://better-hotel-service-1.onrender.com/api/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password }),

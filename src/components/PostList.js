ccbb03cd47e2745d0a90a19119cb4105e6aa7f01
Desalegn Sefiw <166336863+DesuSefiw@users.noreply.github.com>
2025-05-13 15:16:05 +0300

    Update PostList.js

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -54,7 +54,7 @@ const PostList = () => {
   if (!filePath) return null;
 
   // Replace any spaces in filePath with %20
-  const fullPath = `https://better-hotel-service-1.onrender.com${filePath.replace(/ /g, '%20')}`;
+const fullPath = `https://better-hotel-service-1.onrender.com${filePath.replace(/ /g, '%20')}`;
   const fileType = filePath.split('.').pop().toLowerCase();
   console.log('Loading media from:', fullPath);
 

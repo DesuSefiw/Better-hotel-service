36197400761bc78092ad2b7c20375c1713e64da4
Desalegn Sefiw <166336863+DesuSefiw@users.noreply.github.com>
2025-05-21 10:54:23 +0300

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -362,7 +362,7 @@ const handleAddTrainer = async () => {
               <div key={trainer._id} style={{ marginBottom: '10px', padding: '10px', border: '1px solid #ccc', borderRadius: '5px' }}>
                 <p><strong>{trainer.name}</strong> (Email: {trainer.email})</p>
                 <p><strong>Phone-No:</strong> {trainer.phone}</p> 
-                <p><strong>Services:</strong> {trainer.services.join(', ')}</p> {/* Display multiple services */}
+                <p><strong>Services:</strong> {trainer.services.join(', ')}({trainer.type})</p> {/* Display multiple services */}
                 <button onClick={() => handleEditTrainer(trainer)} style={{ padding: '5px', backgroundColor: '#FF9800', color: '#fff', borderRadius: '5px', marginRight: '10px' }}>Edit</button>
                 <button onClick={() => handleDeleteTrainer(trainer._id)} style={{ padding: '5px', backgroundColor: '#f44336', color: '#fff', borderRadius: '5px' }}>Delete</button>
               </div>

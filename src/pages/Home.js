7b48d1b9bff83f683f0a29cde98197fca72d5901
Desalegn Sefiw <166336863+DesuSefiw@users.noreply.github.com>
2025-05-28 23:14:42 -0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -43,6 +43,11 @@ const Home = () => {
         <meta name="keywords" content="hotel booking Ethiopia, Better Hotel, Ethiopian hotels, Addis Ababa hotels, luxury hotel Ethiopia" />
         <meta name="robots" content="index, follow" />
       </Helmet>
+<motion.div 
+  className="animated-bg"
+  animate={{ backgroundPosition: ["0% 0%", "50% 50%", "100% 100%"] }}
+  transition={{ duration: 15, repeat: Infinity, ease: "linear" }}
+/>
 
       <Header />
 
